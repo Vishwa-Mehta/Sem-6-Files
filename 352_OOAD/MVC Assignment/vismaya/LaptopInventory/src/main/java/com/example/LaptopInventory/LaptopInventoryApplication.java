28309fb7ca677e386809dbe5af6878cfc898db0a
Vishwa-Mehta <vishwamehta2002@gmail.com>
2023-04-11 22:45:08 +0530

    mvc assignment done
    Signed-off-by: Vishwa-Mehta <vishwamehta2002@gmail.com>

---
+++ 352_OOAD/MVC Assignment/vismaya/LaptopInventory/src/main/java/com/example/LaptopInventory/LaptopInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.LaptopInventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopInventoryApplication.class, args);
+	}
+
+}
